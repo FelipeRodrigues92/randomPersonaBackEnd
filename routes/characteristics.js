efe707e862eba908c1a53898f40df5270b10577b
FelipeRodrigues92 <liipe.frs@gmail.com>
2020-04-17 03:13:22 -0300

    change mensage

--- routes/characteristics.js
+++ routes/characteristics.js
@@ -75,7 +75,7 @@ router.get('/:quantity',(req,res,next) => {
 
     if(quantity <= 0 ){
         res.status(406).send({
-            mensagem: 'This operation does not accept this params'
+            mensagem: 'This operation does not accept these params'
         });
     }else{
         originString = originArray[getRandomInt(originArray.length)];
