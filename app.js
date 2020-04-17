8243a23e59951f975e9b224d057a3ba29eb8e3ce
FelipeRodrigues92 <liipe.frs@gmail.com>
2020-04-17 00:15:48 -0300

    test heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rotaCharacteristics = require('./routes/characteristics');
 
 app.use('/characteristics', rotaCharacteristics);
 
-app.use('*',(req,res,next) => {
+app.use((req,res,next) => {
     res.status(404).send({
         mensage: 'None router found'
     });
